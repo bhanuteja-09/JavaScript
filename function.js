6f4826947a6f311c315ec3b6b4096f5e738c1ea0
bhanu <bhanutejaswinikaluva@gmail.com>
2024-06-22 20:57:08 +0530

    functions

---
+++ function.js
@@ -0,0 +1,17 @@
+// Function name should not begin with numbers or space or hyphen or back slash or symbals
+// We can give the number between the name and last of the name like good5Start or goodStart3
+//We can use underscore for the function name
+//Function name should be in camel case ex: goodStart
+
+function goodStart(){
+  document.write("Hello World!");
+  console.log("test the code");
+}
+
+//For call the function
+goodStart();
+goodStart();
+goodStart();
+goodStart();
+
+// we call the function as many times we want and whenever we want
---
+++ functions.md
@@ -0,0 +1,15 @@
+# Functions in JavaScript
+
+Keyword   Function Name
+   |        |
+function apple(){   _____
+    statement            |____  Function Block/
+}                   _____|        Function Body
+
+
+1. Function name should not begin with numbers or space or hyphen or back slash or symbols
+
+2. We can give the number between the name and last of the name like good5Start or goodStart3
+3. We can use underscore for the function name
+
+4. Function name should be in camel case ex: goodStart
